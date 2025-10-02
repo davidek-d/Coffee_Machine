14d77ac21b77a9676f2c8df7dc2250f004c31ec3
davidek-d <dajo1002@stud.ek.dk>
2025-10-02 10:43:05 +0200

    wtf

--- src/Coffe_machine/CoffeeMachine.java
+++ src/Coffe_machine/CoffeeMachine.java
@@ -11,6 +11,13 @@ class CoffeeMachine {
         drinks = new ArrayList<>();
         // TODO: Tilføj forskellige drikke til listen
         // Eksempel: drinks.add(new Coffe_machine.Coffee("Sort kaffe"));
+        drinks.add(new Blackcoffee("Black Coffee"));
+        drinks.add(new Espresso("Espresso"));
+        drinks.add(new DoubleEspresso("Double espresso"));
+        drinks.add(new TripleEspresso("Triple espresso"));
+        drinks.add(new LatteMachiato("Latte machiato"));
+        drinks.add(new HotChoclate("Hot chocolate"));
+        drinks.add(new MilkChoclate("Milk chocolate"));
         // TODO: Tilføj Coffe_machine.Espresso og Coffe_machine.ChocolateDrink
     }
 
@@ -21,13 +28,17 @@ class CoffeeMachine {
      */
     public Cup serveDrink(String drinkName, Size size, boolean whippedCream) {
         // TODO: Loop igennem drinks-listen
-        // TODO: Tjek om navnet matcher (ignoreCase)
-        // TODO: Returner resultatet af serve()
-        // TODO: Håndter hvis drikken ikke findes
-        return null;
+        for (Drink d : drinks) {
+            // TODO: Tjek om navnet matcher (ignoreCase)
+            if(drinkName.equalsIgnoreCase(String.valueOf(drinks)))
+
+            // TODO: Returner resultatet af serve()
+            // TODO: Håndter hvis drikken ikke findes
+            return null;
+        }
+
+        // TODO: sortDrinks() – sorter alfabetisk via Comparable
+
+        // TODO: listDrinks() – udskriv alle drikke til konsol
     }
-
-    // TODO: sortDrinks() – sorter alfabetisk via Comparable
-
-    // TODO: listDrinks() – udskriv alle drikke til konsol
 }
